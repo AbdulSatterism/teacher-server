15200e8c84a317e13be1437f5ecf1433afb3be4f
Md. Abdul Satter <116937243+AbdulSatterism@users.noreply.github.com>
2023-06-17 21:54:17 +0600

    jwt

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -20,27 +21,56 @@ const client = new MongoClient(uri, {
     }
 });
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    // console.log(authHeader)
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorize access' })
+    };
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' })
+        };
+        req.decoded = decoded;
+        next();
+    })
+}
+
 async function run() {
     try {
         const serviceCollection = client.db('teacher').collection('services');
         const commentCollection = client.db('teacher').collection('comments');
-        const bookingCollection = client.db('teacher').collection('book')
-        app.post('/services', async (req, res) => {
+        const bookingCollection = client.db('teacher').collection('book');
+
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            res.send({ token })
+        })
+
+        app.post('/services', verifyJWT, async (req, res) => {
             const service = req.body;
             const result = await serviceCollection.insertOne(service);
             res.send(result)
         });
-        app.post('/comment', async (req, res) => {
+        app.post('/comment', verifyJWT, async (req, res) => {
             const comment = req.body;
             const addComment = await commentCollection.insertOne(comment);
             res.send(addComment)
         });
-        app.post('/booking', async (req, res) => {
+        app.post('/booking', verifyJWT, async (req, res) => {
             const booking = req.body;
             const booked = await bookingCollection.insertOne(booking);
             res.send(booked)
         });
-        app.get('/booking', async (req, res) => {
+
+        app.get('/booking', verifyJWT, async (req, res) => {
+            const decoded = req.decoded;
+            // console.log('inside obooking', decoded)
+            if (decoded.email !== req.query.email) {
+                res.status(403).send({ message: 'unauthorize access' })
+            }
             let query = {};
             if (req.query.email) {
                 query = { email: req.query.email }
@@ -50,14 +80,19 @@ async function run() {
             res.send(bookedItems);
         });
         //delete booking
-        app.delete('/booking/:id', async (req, res) => {
+        app.delete('/booking/:id', verifyJWT, async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
             const result = await bookingCollection.deleteOne(query);
             res.send(result)
         })
 
-        app.get('/comment', async (req, res) => {
+        app.get('/comment', verifyJWT, async (req, res) => {
+            const decoded = req.decoded;
+            // console.log('inside obooking', decoded)
+            if (decoded.email !== req.query.email) {
+                res.status(403).send({ message: 'unauthorize access' })
+            }
             let query = {};
             if (req.query.email) {
                 query = { email: req.query.email }
@@ -67,7 +102,7 @@ async function run() {
             res.send(reviews)
         });
         //delete comment 
-        app.delete('/comment/:id', async (req, res) => {
+        app.delete('/comment/:id', verifyJWT, async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
             const result = await commentCollection.deleteOne(query);
